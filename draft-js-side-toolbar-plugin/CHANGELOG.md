8a336ecbfe2ac089df8e2186559051cbc343fdbc
Veselina Kolova <vmkolova@gmail.com>
2018-10-26 17:14:17 +0300

    Initial commit :tada:

---
+++ draft-js-side-toolbar-plugin/CHANGELOG.md
@@ -0,0 +1,23 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+This project adheres to [Semantic Versioning](http://semver.org/).
+
+### 3.1.0
+- Add `scrollParent` prop to help support scroll
+- Add scroll support
+
+### 3.0.0
+- Swap structure prop in side toolbar for render prop
+- Add `position` configuration for positioning left side or right side.
+
+## 2.0.4 - 2.0.5
+- bumped find-with-regex
+
+### 2.0.3
+- Fixed positioning when the editor has a parent node that is positioned (i.e. `relative`, `absolute` or `fixed`).
+
+### 2.0.2
+- accomodate editor dom reference for both react 0.15 and 0.16
+
+### Released the first working of DraftJS Side Toolbar Plugin
---
+++ draft-js-side-toolbar-plugin/src/components/Toolbar/index.js
@@ -0,0 +1,143 @@
+/* eslint-disable react/no-array-index-key */
+import React from 'react';
+import PropTypes from 'prop-types';
+import DraftOffsetKey from 'draft-js/lib/DraftOffsetKey';
+import {
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  BlockquoteButton,
+  CodeBlockButton,
+  UnorderedListButton,
+  OrderedListButton,
+} from 'draft-js-buttons';
+import BlockTypeSelect from '../BlockTypeSelect';
+
+class Toolbar extends React.Component {
+
+  static defaultProps = {
+    children: (externalProps) => (
+      // may be use React.Fragment instead of div to improve perfomance after React 16
+      <div>
+        <HeadlineOneButton {...externalProps} />
+        <HeadlineTwoButton {...externalProps} />
+        <BlockquoteButton {...externalProps} />
+        <CodeBlockButton {...externalProps} />
+        <UnorderedListButton {...externalProps} />
+        <OrderedListButton {...externalProps} />
+      </div>
+    )
+  }
+
+  state = {
+    position: {
+      transform: 'scale(0)',
+    }
+  }
+
+  popupRef = React.createRef();
+
+  componentDidMount() {
+    this.props.store.subscribeToItem('editorState', this.onEditorStateChange);
+  }
+
+  componentWillUnmount() {
+    this.props.store.unsubscribeFromItem('editorState', this.onEditorStateChange);
+  }
+
+  onEditorStateChange = (editorState) => {
+    const selection = editorState.getSelection();
+
+    if (!selection.getHasFocus() && !selection.count()) {
+      this.setState({
+        position: {
+          transform: 'scale(0)',
+        },
+      });
+      return;
+    }
+
+    const currentContent = editorState.getCurrentContent();
+    const currentBlock = currentContent.getBlockForKey(selection.getStartKey());
+    // TODO verify that always a key-0-0 exists
+    const offsetKey = DraftOffsetKey.encode(currentBlock.getKey(), 0, 0);
+    // Note: need to wait on tick to make sure the DOM node has been create by Draft.js
+    setTimeout(() => {
+      const node = document.querySelectorAll(`[data-offset-key="${offsetKey}"]`)[0];
+
+      // The editor root should be two levels above the node from
+      // `getEditorRef`. In case this changes in the future, we
+      // attempt to find the node dynamically by traversing upwards.
+      const editorRef = this.props.store.getItem('getEditorRef')();
+      if (!editorRef) return;
+
+      // this keeps backwards-compatibility with react 15
+      let editorRoot = editorRef.refs && editorRef.refs.editor
+        ? editorRef.refs.editor : editorRef.editor;
+      while (editorRoot.className.indexOf('DraftEditor-root') === -1) {
+        editorRoot = editorRoot.parentNode;
+      }
+
+      const topWithNoScroll = node.offsetTop + editorRoot.offsetTop;
+
+      let valueTop = 0;
+      if (this.props.scrollParent) {
+        const scrollParent = document.querySelectorAll(this.props.scrollParent)[0];
+
+        let element = node;
+        do {
+          valueTop += element.scrollTop || 0;
+          element = element.parentNode;
+        } while (element && element !== scrollParent);
+      }
+
+      const position = {
+        top: topWithNoScroll - valueTop,
+        transform: 'scale(1)',
+        transition: 'transform 0.15s cubic-bezier(.3,1.2,.2,1)',
+      };
+      // TODO: remove the hard code(width for the hover element)
+      if (this.props.position === 'right') {
+        // eslint-disable-next-line no-mixed-operators
+        position.left = editorRoot.offsetLeft + editorRoot.offsetWidth + 80 - 36;
+      } else {
+        position.left = editorRoot.offsetLeft - 80;
+      }
+
+      this.setState({
+        position,
+      });
+    }, 0);
+  }
+
+  onOverrideContent(overrideContent) {
+    this.popupRef.onOverrideContent(overrideContent);
+  }
+
+  render() {
+    const { theme, store, wrapperIcon } = this.props;
+
+    return (
+      <div
+        className={theme.toolbarStyles.wrapper}
+        style={this.state.position}
+      >
+        <BlockTypeSelect
+          getEditorState={store.getItem('getEditorState')}
+          setEditorState={store.getItem('setEditorState')}
+          theme={theme}
+          wrapperIcon={wrapperIcon}
+          ref={this.popupRef}
+        >
+          {this.props.children}
+        </BlockTypeSelect>
+      </div>
+    );
+  }
+}
+
+Toolbar.propTypes = {
+  children: PropTypes.func,
+  wrapperIcon: PropTypes.node
+};
+
+export default Toolbar;
