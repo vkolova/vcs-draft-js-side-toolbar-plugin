92b40cbc8c0bde469004446bfc512fdf27e289ca
Yavor Atov <yavor.atov@gmail.com>
2018-10-29 16:29:48 +0200

    Add script for publishing on s3

--- draft-js-side-toolbar-plugin/publish.sh
+++ draft-js-side-toolbar-plugin/publish.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-npm run build
-cd lib
-tar cvzf vcs-draft-js-side-toolbar-plugin-v18.9.10-1.tar.gz *
-aws s3 cp --acl public-read vcs-draft-js-side-toolbar-plugin-v18.9.10-1.tar.gz "s3://vectorworks-cloud-static/packages/vcs-draft-js-side-toolbar-plugin-v18.9.10-1.tar.gz"
-rm -rf vcs-draft-js-side-toolbar-plugin-v18.9.10-1.tar.gz
+if [ "$#" -ne 1 ]; then
+    echo "usage: ./publish.sh version"
+    exit 1
+fi
 
+version="$1"
+
+npm run build
+tar cvzf "vcs-draft-js-side-toolbar-plugin-${version}.tar.gz" lib package.json
+aws s3 cp --acl public-read "vcs-draft-js-side-toolbar-plugin-${version}.tar.gz" "s3://vectorworks-cloud-static/packages/vcs-draft-js-side-toolbar-plugin-${version}.tar.gz"
+rm -rf "vcs-draft-js-side-toolbar-plugin-${version}.tar.gz"
